26a72e1c9158b71128dc5e5a2fe0fdd677b750dc
Yvor <yvor.vandenbeuken@gmail.com>
2021-02-22 16:00:42 +0100

    Initial commit

---
+++ bluesky-master/bluesky/tools/__init__.py
@@ -0,0 +1,15 @@
+from bluesky import settings
+# Register settings defaults
+settings.set_variable_defaults(prefer_compiled=False)
+if settings.prefer_compiled:
+    try:
+        from . import cgeo as geo
+        print('Using compiled geo functions')
+    except ImportError:
+        from . import geo
+        print('Using Python-based geo functions')
+else:
+    from . import geo
+    print('Using Python-based geo functions')
+
+from . import cachefile
